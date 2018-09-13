a0395686d8d4afc1f388440bf40167b88e684f77
Tayyab <tayyab.bashir26@gmail.com>
2018-09-13 17:10:57 +1000

    adding new line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Testing Stuff");
+        System.out.print("some new stuff");
     }
 }
